c7f9c34b3e635d488fb0cc31aaae563bec3067ff
Chunze-Liu <32441897+15042295553@users.noreply.github.com>
2017-11-02 07:57:25 +0800

    123

---
+++ ConsoleApp1/ConsoleApp1/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Node
+{
+    public object data; // The payload
+    public Node next; // Reference to the next Node in the chain
+
+    public Node()
+    {
+        data = null;
+        next = null;
+    }
+
+    public Node(object data, Node next)
+    {
+        this.data = data;
+        this.next = next;
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,139 @@
 ﻿using System;
 
-namespace ConsoleApp1
+public class Calculator
 {
-    class Program
+    private StackADT stack = new LinkedStack();
+
+    private Scanner scin = new Scanner(System.in);
+
+    
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        
+        Calculator app = new Calculator();
+        bool playAgain = true;
+        Console.WriteLine("\nPostfix Calculator. Recognizes these operators: + - * /");
+        while (playAgain)
         {
-            Console.WriteLine("Hello World!");
+            playAgain = app.doCalculation();
         }
+        Console.WriteLine("Bye.");
     }
-}
+
+    
+    private bool doCalculation()
+    {
+        Console.WriteLine("Please enter q to quit\n");
+        string input = "2 2 +";
+        Console.Write("> ");
+
+        input = scin.nextLine();
+        
+        if (input.StartsWith("q", StringComparison.Ordinal) || input.StartsWith("Q", StringComparison.Ordinal))
+        {
+            return false;
+        }
+        
+        string output = "4";
+        try
+        {
+            output = evaluatePostFixInput(input);
+        }
+        catch (System.ArgumentException e)
+        {
+            output = e.Message;
+        }
+        Console.WriteLine("\n\t>>> " + input + " = " + output);
+        return true;
+    }
+
+    public virtual string evaluatePostFixInput(string input)
+    {
+        if (string.ReferenceEquals(input, null) || input.Equals(""))
+        {
+            throw new System.ArgumentException("Null or the empty string are not valid postfix expressions.");
+        }
+        
+        stack.clear();
+
+        string s; 
+        double a; 
+        double b; 
+        double c; 
+
+        Scanner st = new Scanner(input);
+        while (st.hasNext())
+        {
+            if (st.hasNextDouble())
+            {
+                stack.push(new double?(st.NextDouble())); 
+            }
+            else
+            {
+                
+                s = st.next();
+                if (s.Length > 1)
+                {
+                    throw new System.ArgumentException("Input Error: " + s + " is not an allowed number or operator");
+                }
+                
+                if (stack.Empty)
+                {
+                    throw new System.ArgumentException("Improper input format. Stack became empty when expecting second operand.");
+                }
+                b = ((double?)(stack.pop())).Value;
+                if (stack.Empty)
+                {
+                    throw new System.ArgumentException("Improper input format. Stack became empty when expecting first operand.");
+                }
+                a = ((double?)(stack.pop())).Value;
+                
+                c = doOperation(a, b, s);
+                
+                stack.push(new double?(c));
+            }
+        } 
+        return ((double?)(stack.pop())).ToString();
+    }
+
+
+    
+    public virtual double doOperation(double a, double b, string s)
+    {
+        double c = 0.0;
+        if (s.Equals("+"))
+        {
+            c = (a + b);
+        }
+        else if (s.Equals("-"))
+        {
+            c = (a - b);
+        }
+        else if (s.Equals("*"))
+        {
+            c = (a * b);
+        }
+        else if (s.Equals("/"))
+        {
+            try
+            {
+                c = (a / b);
+                if (c == double.NegativeInfinity || c == double.PositiveInfinity)
+                {
+                    throw new System.ArgumentException("Can't divide by zero");
+                }
+            }
+            catch (ArithmeticException e)
+            {
+                throw new System.ArgumentException(e.Message);
+            }
+        }
+        else
+        {
+            throw new System.ArgumentException("Improper operator: " + s + ", is not one of +, -, *, or /");
+        }
+
+        return c;
+    }
+
+} 
