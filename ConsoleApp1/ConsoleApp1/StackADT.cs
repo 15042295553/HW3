45ea5047270c5dbac924a6a5db70336d3c63a17b
Chunze-Liu <32441897+15042295553@users.noreply.github.com>
2017-11-02 08:35:05 +0800

    666

---
+++ ConsoleApp1/ConsoleApp1/StackADT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public interface StackADT
+{
+    
+    object push(object newItem);
+
+    
+    object pop();
+
+    
+    object peek();
+
+    
+    bool Empty { get; }
+
+    
+    void clear();
+}
